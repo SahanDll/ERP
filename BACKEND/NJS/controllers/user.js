4dc41697bcbacd01acbe80f7a9b5886ee9f6d5fd
Sahan Devaka Lokuge <sahandevaka@gmail.com>
2018-03-19 17:51:24 +0800

    data fetch changed

--- BACKEND/NJS/controllers/user.js
+++ BACKEND/NJS/controllers/user.js
@@ -1,47 +1,40 @@
 var express = require('express');
 var router = express.Router();
 var mongo = require('../data_source/mongo');
+var UserData = mongo.getUserData();
 
 router.get('/get-user-all', function (req, res, next) {
-    var UserData = mongo.getUserData();
-
     UserData.find()
-        .then(function (err, doc) {
-            if(err){
-                res.send(JSON.stringify(err));
-            }else {
+        .then(function (doc) {
+            if(doc){
                 res.send(JSON.stringify(doc));
+            }else {
+                res.json({error: 'Server Error'});
             }
         })
 });
 
 router.get('/get-user-name', function (req, res, next) {
-    var UserData = mongo.getUserData();
-
     UserData.find({ userName: req.query.userName})
-        .then(function (err, doc) {
-            if(err){
-                res.send(JSON.stringify(err));
-            }else {
+        .then(function (doc) {
+            if(doc){
                 res.send(JSON.stringify(doc));
+            }else {
+                res.json({error: 'User not found'});
             }
         })
 });
 
 router.post('/add-user', function (req, res, next) {
-    console.log("1")
     var user = {
         userName: req.body.userName,
         password: req.body.password,
         role: req.body.role,
         created: new Date()
     };
-    console.log("2")
     var data = mongo.createUserData(user);
-    console.log("3")
     data.save();
-    console.log(data)
-    res.send(JSON.stringify(data));
+    res.json({message: 'User created successfully'});
 });
 
 
